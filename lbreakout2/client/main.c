93766601eb985896e547238003f931f95f7c4f21
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2010-10-05 09:24:10 +0000

    added some info
    git-svn-id: https://svn.code.sf.net/p/lgames/code/trunk@8 b357a508-0cf7-4c0e-b265-6d29fad4a9a1

--- lbreakout2/client/main.c
+++ lbreakout2/client/main.c
@@ -91,8 +91,9 @@ int main(int argc, char *argv[])
 #endif    
     
     /* lbreakout info */
-    printf( "LBreakout2 %s\nCopyright 2001-2005 Michael Speck\nPublished under GNU GPL\n---\n", VERSION );
+    printf( "LBreakout2 %s\nCopyright 2001-2010 Michael Speck\nPublished under GNU GPL\n---\n", VERSION );
     printf( "Looking up data in: %s\n", SRC_DIR );
+    printf( "Looking up highscores in: %s\n", HI_DIR );
     printf( "Looking up custom levels in: %s/%s/lbreakout2-levels\n", (getenv( "HOME" )?getenv( "HOME" ):"."), CONFIG_DIR_NAME );
 #ifndef AUDIO_ENABLED
     printf( "Compiled without sound and music\n" );
