f6fc90ff99906618967127f21146a429a54a2070
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2011-06-19 18:27:07 +0000

    add version to archive and title
    git-svn-id: https://svn.code.sf.net/p/lgames/code/trunk@82 b357a508-0cf7-4c0e-b265-6d29fad4a9a1

--- jwt/makedist.sh
+++ jwt/makedist.sh
@@ -4,14 +4,17 @@ version=$1
 [ x$version = x ] && echo "Version missing!" && exit 1
 
 path=$(pwd)
-dest=/tmp/jwt
+dest=/tmp/jwt-${version}
 
 rm -rf $dest
 mkdir $dest $dest/pali
 cp pali/*.js $dest/pali
-cp changelog.txt copying.txt readme.txt 
-cp jwt.js dialog.js pali.htm style.css $dest
+cp changelog.txt copying.txt readme.txt $dest 
+cp jwt.js dialog.js style.css $dest
+cat pali.htm | sed "s/#VERSION#/${version}/g" > $dest/pali.htm
+
+rm -f $path/jwt-${version}.zip
 cd /tmp
-zip -r $path/jwt-${version}.zip jwt
+zip -r $path/jwt-${version}.zip jwt-${version}
 rm -rf $dest
 exit 0
