ae1606ab43e43106b32b4ca0449985dada58344a
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2016-06-12 11:52:47 +0000

    fixed broken php mark in credits
    git-svn-id: https://svn.code.sf.net/p/lgames/code/trunk@173 b357a508-0cf7-4c0e-b265-6d29fad4a9a1

--- occ/images/beholder/credits.php
+++ occ/images/beholder/credits.php
@@ -1,2 +1,2 @@
-<? $credits='The chessmen graphics were taken from <A class="sublink" href="http://sourceforge.net/projects/webchess/">WebChess</A>.';?>
+<?php $credits='The chessmen graphics were taken from <A class="sublink" href="http://sourceforge.net/projects/webchess/">WebChess</A>.';?>
 
--- occ/images/default/credits.php
+++ occ/images/default/credits.php
@@ -1,2 +1,2 @@
-<? $credits='The chessmen graphics were taken from <A class="tiny" href="http://www.tommasovitale.it/my/wcg">WCG</A> with kind permission of Tommaso Vitale.';?>
+<?php $credits='The chessmen graphics were taken from <A class="tiny" href="http://www.tommasovitale.it/my/wcg">WCG</A> with kind permission of Tommaso Vitale.';?>
 
